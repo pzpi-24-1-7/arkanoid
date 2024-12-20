23cb0ba123cbcd9bd685721ea139cc6e8e632e2f
vadrille <109307947+vadrille12@users.noreply.github.com>
2024-12-20 09:21:26 +0200

    random vx

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -5,5 +5,16 @@ export default class Ball {
     this.vx = vx;
     this.vy = vy;
     this.r = r;
+    this.setRandomVelocityX();
+  }
+
+  setRandomVelocityX() {
+    const isNegativeRange = Math.random() < 0.5;
+
+    if (isNegativeRange) {
+      this.vx = Math.random() * (-2 - -3) + -3;
+    } else {
+      this.vx = Math.random() * (3 - 2) + 2;
+    }
   }
 }
